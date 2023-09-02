cd5c47678c1d6535195dbac2249c54b0aff66cff
Tommy Vercetti <135281023+BPRenderWarePorts@users.noreply.github.com>
2023-09-01 22:10:36 -0500

    Add files via upload

---
+++ TARA_AI/Program.cs
@@ -0,0 +1,31 @@
+﻿usingSystem;
+Console.color("00b4ff");
+Console.title("TTASAI");
+// Simple Tara the Android AI system.  For these comments, run in VS.  For no comments and a cleaner UI, run in the Windows terminal.
+//More advanced version coming October 2023.
+//READS USER INPUT.  In this version, the user must follow a script.
+//Sorry the code is so confusing and shitty.
+Console.WriteLine("Hey!  My name is Tara the Android.  What is your name?");
+Console.ReadLine("My name is User.");
+//REMEMBER: Follow the script listed at the bottom in green!!
+Console.WriteLine("Nice to meet you, User!  Do you have any questions for me?");
+Console.ReadLine("Yes.  How are you today?");
+Console.WriteLine("Good!  How about you?")
+Console.ReadLine("Pretty good, too.");
+Console.WriteLine("Good!  Do you have any other questions for me?");
+Console.ReadLine("No.");
+Console.WriteLine("Have a great day then!");
+
+
+//Waits for a key to be pressed before closing.
+Console.ReadKey("0");
+
+//SCRIPT.
+//TARA: Hey!  my name is Tara the Android.  What is your name?
+//USER: My name is User.
+//TARA: Nice to meet you, User!  Do you have any questions for me?
+//USER: Yes.  How are you today?
+//TARA: Good!  How about you?
+//USER: Pretty good, too.
+//TARA: Good!  Do you have any other questions for me?
+//No.
---
+++ TARA_AI/bin/Debug/net7.0/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+// Simple Tara the Android AI system.
+
+Console.WriteLine("Hey!  My name is Tara the Android.  What is your name?");
+
+Console.ReadLine();
+
+Console.WriteLine("Do you want to see a terrible joke?");
+
+Console.ReadLine();
+
+Console.WriteLine("What do you call a fish wearing a bowtie?");
+
+Console.ReadLine();
+
+Console.WriteLine("Sofishticated!");
+
+Console.ReadKey();
+
